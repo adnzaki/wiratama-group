57cd1719c3c15f5f8d305b1c39346eb645a32a5c
Adnan Zaki <adnzaki@outlook.com>
2024-10-25 22:02:01 +0700

    fix: add target link on hero button

--- app/Views/home/hero.php
+++ app/Views/home/hero.php
@@ -1,41 +1,41 @@
 <!--===== HERO AREA STARTS =======-->
 <div class="header-carousel-area3 owl-carousel">
-    <?php foreach($heros as $hero): ?>
-    <div class="main-hero-area">
-        <div class="img1">
-            <img width="900" height="844" src="<?= $img ?>all-images/hero/<?= $hero['image'] ?>"  alt="Hero slider images">
-        </div>
-        <div class="bg-elements">
-            <img src="<?= $img ?>elements/elements7.png" alt="" class="elements2">
-        </div>
-        <div class="container" style="margin-top: -70px;"  >
-            <div class="row">
-                <div class="col-lg-5 col-md-9">
-                    <div class="header-content-area header-heading">
-                        <div class="elements">
-                            <img src="<?= $img ?>elements/elements3.png" alt="">
-                        </div>
-                        <div class="header-heading2">
-                            <h5><i class="fa-solid fa-location-dot"></i><?= $hero['address'] ?></h5>
-                            <div class="space20"></div>
+    <?php foreach ($heros as $hero): ?>
+        <div class="main-hero-area">
+            <div class="img1">
+                <img width="900" height="844" src="<?= $img ?>all-images/hero/<?= $hero['image'] ?>" alt="Hero slider images">
+            </div>
+            <div class="bg-elements">
+                <img src="<?= $img ?>elements/elements7.png" alt="" class="elements2">
+            </div>
+            <div class="container" style="margin-top: -70px;">
+                <div class="row">
+                    <div class="col-lg-5 col-md-9">
+                        <div class="header-content-area header-heading">
+                            <div class="elements">
+                                <img src="<?= $img ?>elements/elements3.png" alt="">
+                            </div>
+                            <div class="header-heading2">
+                                <h5><i class="fa-solid fa-location-dot"></i><?= $hero['address'] ?></h5>
+                                <div class="space20"></div>
 
-                            <h2><?= $hero['tagline'] ?></h2>
-                            
-                            <div class="space20"></div>
-                            <p><?= $hero['description'] ?></p>
-                            <div class="space32"></div>
-                            <div class="btn-area1">
-                                <a href="<?= $hero['buttonUrl'] ?>" onclick="getStarted()" class="header-btn3">Mulai Jelajahi</a>
-                                <!-- <a href="https://www.youtube.com/watch?v=Y8XpQpW5OVY" class="popup-youtube">
+                                <h2><?= $hero['tagline'] ?></h2>
+
+                                <div class="space20"></div>
+                                <p><?= $hero['description'] ?></p>
+                                <div class="space32"></div>
+                                <div class="btn-area1">
+                                    <a href="#property-overview" onclick="getStarted()" class="header-btn3">Mulai Jelajahi</a>
+                                    <!-- <a href="https://www.youtube.com/watch?v=Y8XpQpW5OVY" class="popup-youtube">
                                     <span class="play-btn"><i class="fa-solid fa-play"></i></span>
                                     <span class="text">Video</span>
                                 </a> -->
+                                </div>
                             </div>
                         </div>
                     </div>
-                </div>
-                <div class="col-lg-2"></div>
-                <!-- <div class="col-lg-4">
+                    <div class="col-lg-2"></div>
+                    <!-- <div class="col-lg-4">
                     <div class="auhtor-box">
                         <div class="others-box">
                             <div class="img3">
@@ -49,9 +49,9 @@
                         </div>
                     </div>
                 </div> -->
+                </div>
             </div>
         </div>
-    </div>
     <?php endforeach; ?>
 
 </div>
